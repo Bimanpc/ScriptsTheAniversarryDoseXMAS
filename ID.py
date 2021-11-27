873f1709e4a1138b1e82ba29503594491b290368
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-11-27 19:27:28 +0200

    Add files via upload

---
+++ ID.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+
+__author__ = 'pctechgreu.rf.gd'
+
+personInfo = ("Bill PSaromatis", 20, "Athens")
+print(personInfo[0])
+print(personInfo[1])
---
+++ List.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+ 
+__author__ = 'pctechgreu.rf.gd'
+
+l = [ "Psaromatis", "Lambros", "Bill", "Koula" ]
+ 
+print(l)     # prints all elements
+print(l[0])  # print first element
+print(l[1])  # prints second element
