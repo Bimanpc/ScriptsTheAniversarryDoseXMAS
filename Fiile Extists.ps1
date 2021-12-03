564dc0b18c162b91f9629e8e8c454ef8b62a7a6c
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-12-03 19:20:30 +0200

    Add files via upload

---
+++ Fiile Extists.ps1
@@ -0,0 +1,11 @@
+﻿$FileName = (Get-Date).tostring("dd-MM-yyyy")
+$FolderPath="C:\Desktop"
+$Path = $FolderPath+"\"+ $FileName+".txt"
+if (!(Test-Path $Path))
+{
+New-Item -itemType File -Path $FolderPath -Name ($FileName + ".txt")
+}
+else
+{
+Write-Host "File Exists"
+}
