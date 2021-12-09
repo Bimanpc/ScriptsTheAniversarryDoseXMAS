dafd3e02b5525e753ee334d08a4bb378adb056ab
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-12-09 19:23:29 +0200

    Add files via upload

---
+++ Talk Me AI.bat
@@ -0,0 +1,8 @@
+@echo off
+title My GekkeITMzh Batch Fille
+color 03
+echo Anything you want to say goes here.
+pause
+echo Continue however many times desired.
+pause
+quit
---
+++ auth.bat
@@ -0,0 +1,18 @@
+@echo off
+title Authenticate
+echo Welcome Geekzh.
+pause
+echo Let me connect you.
+pause
+echo Loading.
+ping localhost -n 2 >nul
+cls
+echo Loading..
+ping localhost -n 2 >nul
+cls
+echo Loading...
+ping localhost -n 2 >nul
+cls
+echo Connected
+pause
+quit
