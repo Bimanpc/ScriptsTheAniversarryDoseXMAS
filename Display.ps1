0efe1c494cb7f870eed2c752c838ddfbacc8c6a8
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-12-18 19:20:03 +0200

    Add files via upload

---
+++ Display.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Dispalying out grid in a GUI with search" -ForegroundColor Green
+$pro_arry = @()
+$processes = Get-Process | Select Name, Id, CPU, SI
+Foreach ($process in $processes){
+$pro_arry += New-Object PSObject -Property @{
+Process_Name = $process.Name
+Process_CPU = $process.CPU
+Process_Id = $process.Id
+Process_SI = $process.SI
+}
+}
+$op = $pro_arry | Out-GridView -Title "Custom grid with Filters" -OutputMode Multiple
---
+++ inPut.ps1
@@ -0,0 +1,30 @@
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+$inputform = New-Object System.Windows.Forms.Form
+$inputform.Text = "PCTECGREU"
+$inputform.Size = New-Object System.Drawing.Size(300,300)
+$inputform.StartPosition = "CenterScreen"
+$OKB = New-Object System.Windows.Forms.Button
+$OKB.Location = New-Object System.Drawing.Size(65,120)
+$OKB.Size = New-Object System.Drawing.Size(65,23)
+$OKB.Text = "Submit"
+$OKB.Add_Click({Write-Host "Entered value is"$OTB.Text -ForegroundColor Green;$inputform.Close()})
+$inputform.Controls.Add($OKB)
+$CAB = New-Object System.Windows.Forms.Button
+$CAB.Location = New-Object System.Drawing.Size(140,120)
+$CAB.Size = New-Object System.Drawing.Size(65,23)
+$CAB.Text = "Clear"
+$CAB.Add_Click({Write-Host "No input from user" -ForegroundColor Red;$inputform.Close()})
+$inputform.Controls.Add($CAB)
+$Lbl = New-Object System.Windows.Forms.Label
+$Lbl.Location = New-Object System.Drawing.Size(10,20)
+$Lbl.Size = New-Object System.Drawing.Size(260,20)
+$Lbl.Text = "Please provide a value
+$inputform.Controls.Add($Lbl)
+$OTB = New-Object System. .Forms.TextBox
+$OTB.Location = New-Object System.Drawing.Size(10,50)
+$OTB.Size = New-Object System.Drawing.Size(240,20)
+$inputform.Controls.Add($OTB)
+$inputform.Topmost = $True
+$inputform.Add_Shown({$inputform.Activate()})
+[void] $inputform.ShowDialog()
