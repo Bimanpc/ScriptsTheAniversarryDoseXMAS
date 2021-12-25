a1b3d264e1c5f6a2d71609dee30803830774c523
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2021-12-25 19:45:14 +0200

    Add files via upload

---
+++ XMAS LOVE.ps1
@@ -0,0 +1,12 @@
+﻿$Question = $host.ui.PromptForChoice(
+    "Window Title", "Do You Like XMAS?",(
+        [System.Management.Automation.Host.ChoiceDescription[]](
+            (New-Object System.Management.Automation.Host.ChoiceDescription "&YES","NO"),
+            (New-Object System.Management.Automation.Host.ChoiceDescription "&YES","NO"),
+        )
+    ), 0
+) 
+switch($Question){
+    0 {Write-Host "YES"}
+    1 {Write-Host "NO"}
+}
