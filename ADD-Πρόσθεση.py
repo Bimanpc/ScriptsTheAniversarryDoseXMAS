45141b6e27cff33d693eb93f4265412a284ecff1
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-01-05 19:37:34 +0200

    Add files via upload

---
+++ ADD-Πρόσθεση.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+__author__ = 'pctechgreu'
+
+x = int(input("Enter x:"))
+y = int(input("Enter y:"))
+
+sum = x + y
+print(sum)
---
+++ biLIger.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+ 
+__author__ = 'pctechgreu'
+
+s = "Hello Python Hello Geeks"
+print(s)      # prints whole string
+print(s[0])   # prints "H"
+print(s[1]) # prints "e"
