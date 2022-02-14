8674585b7345e18f4d4030c883320e7e17dee106
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-02-14 19:42:03 +0200

    Add files via upload

---
+++ Hi BillyMan.ps1
@@ -0,0 +1,28 @@
+﻿Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text = "PCTEGREU Geeks"
+$form.Size = New-Object System.Drawing.Size(150, 145)
+$form.AutoSize = $true
+
+$lbl1 = New-Object System.Windows.Forms.Label
+$lbl1.Text = "Hello World Hello Geeks!"
+$lbl1.Location = New-Object System.Drawing.Point(30, 20);
+
+$lbl1.Add_Click({
+    [System.Windows.Forms.MessageBox]::Show("Hey!Hi!")
+})
+
+$btn = New-Object System.Windows.Forms.Button
+$btn.Text = "Close"
+$btn.location = New-Object System.Drawing.Point(30, 60);
+$btn.DialogResult = [System.Windows.Forms.DialogResult]::OK
+
+$form.AcceptButton = $btn
+$form.controls.Add($lbl1)
+$form.controls.Add($btn)
+
+$Null = $form.ShowDialog()
