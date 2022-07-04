e36c7a11f60f5a095904bb237146546bccf8046a
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-07-04 19:47:29 +0300

    Add files via upload

---
+++ Geekz.bat
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "************************************************"
+echo "Hello Geekzzz!"
+echo "Learn to print out Hello World"
+echo "************************************************"
+echo "Powered by Billys Geekzzz"
+echo "################################################"
+echo "Printing : Hello World!!!!!"
