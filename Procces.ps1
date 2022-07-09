0e7668dfc245f977e0e26df78631c5bb968a4516
Βασίλης Ψαρομάτης PC TECH GR EU BIILMAN <77292453+Bimanpc@users.noreply.github.com>
2022-07-09 19:28:18 +0300

    Add files via upload

---
+++ Procces.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Demo of dispalying out off grid in a GUI with search" -ForegroundColor Green
+$pro_arry = @()
+$processes = Get-Process | Select Name, Id, CPU, SI
+Foreach ($process in $processes){
+$pro_arry += New-Object PSObject -Property @{
+Process_Name = $process.Name
+Process_CPU = $process.CPU
+Process_Id = $process.Id
+Process_SI = $process.SI
+}
+}
+$op = $pro_arry | Out-GridView -Title "Custome grid with Filters" -OutputMode Multiple
