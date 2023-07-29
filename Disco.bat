bfc2faf6d9790464002c9c5569f173b32b581009
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-07-29 19:48:43 +0300

    Add files via upload

---
+++ Disco.bat
@@ -0,0 +1,16 @@
+@echo off
+color 0A
+
+:disco
+cls
+echo Dancing Disco Lights
+ping -n 1 127.0.0.1 >nul
+color 0B
+ping -n 1 127.0.0.1 >nul
+color 0C
+ping -n 1 127.0.0.1 >nul
+color 0D
+ping -n 1 127.0.0.1 >nul
+color 0E
+ping -n 1 127.0.0.1 >nul
+goto disco
