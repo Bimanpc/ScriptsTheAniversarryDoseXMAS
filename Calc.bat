0ee64b19aac1125a0d38288eb818ad8d008cf1ff
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2023-09-30 19:39:57 +0300

    Add files via upload

---
+++ Calc.bat
@@ -0,0 +1,12 @@
+SET /A sum=1+1     ::addition operator
+ECHO %sum%     
+SET /A mul=7*9     ::multiplication operator
+ECHO %mul%
+SET /A div=9/3     ::Division operator
+ECHO %div%
+SET /A assign=10   ::Assignment operator
+ECHO %assign%
+SET /A assign+=15  ::Increment then assignment operator
+ECHO %assign%
+SET /A mod= 10%3   ::Modulus/Remainder operator
+ECHO %mod%
