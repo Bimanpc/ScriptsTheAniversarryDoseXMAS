b63095dc6fc1566b5e1da56b29cc31fae2b2526e
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2024-11-27 19:32:40 +0200

    xmas2024.bat

---
+++ xmas2024.bat
@@ -0,0 +1,20 @@
+@echo off
+:: Hide the cursor
+echo [32m[?25l
+
+:: Set title
+title Merry Christmas!
+
+:: Colors and ASCII Art
+cls
+echo.
+echo. Merry Christmas!
+echo.
+echo [34m
+echo                *    
+echo               /#\   
+echo              /###\  
+echo             /#####\ 
+echo            /##[31mO[34]##\ 
+echo           /#########\  
+echo          /##[31mO[34###@ 
