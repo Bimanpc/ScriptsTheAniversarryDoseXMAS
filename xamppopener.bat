02b67d4d3020e8d253b1596ac1825b024faddaf0
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS <77292453+Bimanpc@users.noreply.github.com>
2025-05-10 19:15:35 +0300

    xamppopener.bat

---
+++ xamppopener.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Path to your Apache bin directory
+cd C:\xampp\apache\bin
+
+REM Command to start Apache
+httpd.exe
+
+REM Optional: Display a message
+echo Apache server is starting...
