4d8e5cd217426b55f9f3e15b4ec9c5086e717417
Βασίλης Ψαρομάτης PC TECH GR EU Vasiliy PS-##VPSAIDEV## <77292453+Bimanpc@users.noreply.github.com>
2026-03-27 19:10:01 +0200

    Create equal1.cpp

---
+++ equal1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std
+main( )
+{
+int number = 1, count = 0;
+while (number != 0)
+{
+cout << “∆ώσε έναν ακέραιο αριθµό (0 για μηδενισμό) : “ <<
+endl;
+cin >> number);
+count++;
+}
+cout << “Το πλήθος των αριθµών που δόθηκαν είναι ” << count;
+}
